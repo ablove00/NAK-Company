9a0b95f3a33a790b4e3a6d1bf0f958026744f978
ABStudio <ablove00@gmail.com>
2025-08-11 20:06:51 +0330

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../stores/authStore";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://nak-interview.darkube.app",
 });
 
 api.interceptors.request.use((config) => {
